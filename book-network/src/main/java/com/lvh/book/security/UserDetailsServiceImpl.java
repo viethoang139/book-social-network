ad1743bf43f36ae302f985717bbd70a0261b0a3a
Le Viet Hoang <levieth710@gmail.com>
2024-04-22 21:31:46 +0700

    first-commit

---
+++ book-network/src/main/java/com/lvh/book/security/UserDetailsServiceImpl.java
@@ -0,0 +1,21 @@
+package com.lvh.book.security;
+
+import com.lvh.book.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
